ce96faae4ba66ee8736ec3913a104ec6daf70230
Rick Campbell <agentofmisfortune@gmail.com>
2019-11-11 00:39:33 -0500

    Updates

---
+++ precache-manifest.b04fdd252cd9e21bb340f8e68836e667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a4bc25cdc26798d1f29c88c133b4ec",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "ae5d710813bd7ae5e675",
+    "url": "/smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "245a04c0c6064f47c164",
+    "url": "/smartbrain/static/css/main.f050e911.chunk.css"
+  },
+  {
+    "revision": "ae5d710813bd7ae5e675",
+    "url": "/smartbrain/static/js/2.7cbd28e2.chunk.js"
+  },
+  {
+    "revision": "245a04c0c6064f47c164",
+    "url": "/smartbrain/static/js/main.aad28db9.chunk.js"
+  },
+  {
+    "revision": "7dddf3a2d903280cd8c0",
+    "url": "/smartbrain/static/js/runtime-main.ce739238.js"
+  }
+]);
